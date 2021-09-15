3655950a782c51a2a0148b81741529d14e4ebde7
peerapatch <caramelza@hotmail.com>
2021-09-15 15:28:06 +0700

    Game Logic

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{useState,useRef,useEffect} from 'react';
 
 export default function CharacterCard(props) { 
     const [active, setActive] = useState(false); 
@@ -7,11 +7,21 @@ export default function CharacterCard(props) {
              setActive(true)             
              props.activationHandler(props.value)  
     }
+    setActive(true)
 }
     const className = `card${active ? ' activeCard': ''}`   
+
+    const attemptRef = useRef(props.attempt);
+    useEffect(() => {
+        if(attemptRef.current != props.attempt){
+        setActive(false)
+        attemptRef.current = props.attempt
+        }
+        })
+       
     return (
         <div className={className} onClick={activate}>
             {props.value}
-        </div>     )   
+        </div> )   
          
         }
